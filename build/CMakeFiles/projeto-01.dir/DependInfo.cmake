d0443d94998cfc83f113b2fd7c2de4e27296f11e
Ruhan <diegoruhan@gmail.com>
2025-05-05 11:13:47 -0300

    Criado código e feito testes.

---
+++ build/CMakeFiles/projeto-01.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/aluno/CLionProjects/Teste-GIT/contador.c" "CMakeFiles/projeto-01.dir/contador.c.o" "gcc" "CMakeFiles/projeto-01.dir/contador.c.o.d"
+  "/home/aluno/CLionProjects/Teste-GIT/main.c" "CMakeFiles/projeto-01.dir/main.c.o" "gcc" "CMakeFiles/projeto-01.dir/main.c.o.d"
+  "/home/aluno/CLionProjects/Teste-GIT/util.c" "CMakeFiles/projeto-01.dir/util.c.o" "gcc" "CMakeFiles/projeto-01.dir/util.c.o.d"
+  "" "projeto-01" "gcc" "CMakeFiles/projeto-01.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/projeto-01.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projeto-01.dir/link.d"
+  "CMakeFiles/projeto-01.dir/contador.c.o"
+  "CMakeFiles/projeto-01.dir/contador.c.o.d"
+  "CMakeFiles/projeto-01.dir/main.c.o"
+  "CMakeFiles/projeto-01.dir/main.c.o.d"
+  "CMakeFiles/projeto-01.dir/util.c.o"
+  "CMakeFiles/projeto-01.dir/util.c.o.d"
+  "projeto-01"
+  "projeto-01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projeto-01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
